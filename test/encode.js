26d7e1bc132f5f78dbdff16fffb58b96a6cfc36c
Robert Keizer <robert@keizer.ca>
2018-09-15 12:03:36 -0500

    Exposing some defaults and options.

--- test/encode.js
+++ test/encode.js
@@ -2,9 +2,7 @@ const Encoder = require( "../lib/encoder" );
 
 describe( "Encoder", function( ){
 	it( "Emits a ready event", function( cb ){
-		const encoder = new Encoder( {
-			version: "1"
-		} );
+		const encoder = new Encoder( );
 		encoder.once( "ready", function( ){
 			return cb( null );
 		} );
@@ -12,9 +10,7 @@ describe( "Encoder", function( ){
 
 	it( "Calling encode results in data being emitted", function( cb ){
 		this.timeout( 30000 );
-		const encoder = new Encoder( {
-			version: "1"
-		} );
+		const encoder = new Encoder( );
 		encoder.once( "ready", function( ){
 			encoder.encode( "Test String" );
 		} );
