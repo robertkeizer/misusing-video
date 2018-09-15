e331f2dff1c2517ad221ca20e500aca84fca7520
Robert Keizer <robert@keizer.ca>
2018-09-15 11:51:41 -0500

    WIP: have qrencode being spawned.

--- test/encode.js
+++ test/encode.js
@@ -1,11 +1,28 @@
 const Encoder = require( "../lib/encoder" );
 
 describe( "Encoder", function( ){
-	it.only( "Emits a ready event", function( cb ){
+	it( "Emits a ready event", function( cb ){
 		const encoder = new Encoder( {
 			version: "1"
 		} );
 		encoder.once( "ready", function( ){
+			return cb( null );
+		} );
+	} );
+
+	it.only( "Calling encode results in data being emitted", function( cb ){
+		this.timeout( 30000 );
+		const encoder = new Encoder( {
+			version: "1"
+		} );
+		encoder.once( "ready", function( ){
+			console.log( "Calling encode" );
+			encoder.encode( "Test String" );
+		} );
+
+		encoder.once( "data", function( data ){
+			console.log( "I have data of ");
+			console.log( data );
 			return cb( null );
 		} );
 	} );
