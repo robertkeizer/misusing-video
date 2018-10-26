8b4c0e355c2b08147244455cc208fe9794288260
Robert Keizer <robert@keizer.ca>
2018-10-25 20:20:49 -0500

    Trying to get cleanup and shutdown going properly.

--- test/encode.js
+++ test/encode.js
@@ -1,23 +1,41 @@
 const Encoder = require( "../lib/encoder" );
 
-describe.only( "Encoder", function( ){
+describe( "Encoder", function( ){
 	it( "Emits a ready event", function( cb ){
 		const encoder = new Encoder( );
 		encoder.once( "ready", function( ){
-			return cb( null );
+			encoder.shutdown( cb );
 		} );
 	} );
 
 	it( "Calling encode results in data being emitted", function( cb ){
 		this.timeout( 30000 );
 		const encoder = new Encoder( );
+		
 		encoder.once( "ready", function( ){
 			encoder.encode( "Test String" );
 		} );
 
 		encoder.once( "data", function( data ){
-			console.log( data );
-			return cb( null );
+			setTimeout( ( ) => {
+				return encoder.shutdown( cb );
+			}, 5000 );
+		} );
+	} );
+
+	it.only( "Calling encode and then calling .finishCurrentFragment results in fragment being emitted.", function( cb ){
+		this.timeout( 30000 );
+		const encoder = new Encoder( );
+		
+		encoder.once( "ready", function( ){
+			encoder.once( "fragment", function( data ){
+				console.log( " ihave data of " );
+				console.log( data );
+				encoder.shutdown( cb );
+			} );
+			encoder.encode( "Test String" );
+
+			encoder.finishCurrentFragment( ( err ) => { });
 		} );
 	} );
 } );
